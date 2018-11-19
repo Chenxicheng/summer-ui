952087384895d2b3aa50a2cba68b1c47c8859dfa
Chenxicheng <850068241@qq.com>
2018-11-19 16:40:06 +0800

    修复面包屑bug

--- src/router/routers.js
+++ src/router/routers.js
@@ -8,6 +8,7 @@ import Main from '@/components/main'
  *         显示在侧边栏、面包屑和标签栏的文字
  *         使用'{{ 多语言字段 }}'形式结合多语言使用，例子看多语言的路由配置;
  *         可以传入一个回调函数，参数是当前路由对象，例子看动态路由和带参路由
+ *  hideInBread: (false) 设为true后此级路由将不会出现在面包屑中，示例看QQ群路由配置
  *  hideInMenu: (false) 设为true后在左侧菜单不会显示该页面选项
  *  notCache: (false) 设为true后页面不会缓存
  *  access: (null) 可访问该页面的权限数组，当前路由设置的权限会影响子路由
@@ -56,7 +57,7 @@ export const routes = [
     path: '/error_store',
     name: 'error_store',
     meta: {
-      hide: true
+      hideInBread: true
     },
     component: Main,
     children: [
@@ -75,7 +76,7 @@ export const routes = [
     path: '/error_logger',
     name: 'error_logger',
     meta: {
-      hide: true,
+      hideInBread: true,
       hideInMenu: true
     },
     component: Main,
