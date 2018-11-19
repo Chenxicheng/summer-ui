0d798e588c8d640f37db9006da74739814584da0
Chenxicheng <850068241@qq.com>
2018-11-19 14:52:40 +0800

    update

--- src/store/module/app.js
+++ src/store/module/app.js
@@ -43,10 +43,10 @@ export default {
       let tagList = []
       if (list) {
         tagList = [...list]
-      } else tagList = getTagNavListFromLocalstorage()
-      if (tagList[0].name !== homeName) tagList.shift()
+      } else tagList = getTagNavListFromLocalstorage() || []
+      if (tagList[0] && tagList[0].name !== homeName) tagList.shift()
       let homeTagIndex = tagList.findIndex(item => item.name === homeName)
-      if (homeTagIndex !== 0) {
+      if (homeTagIndex > 0) {
         let homeTag = tagList.splice(homeTagIndex, 1)[0]
         tagList.unshift(homeTag)
       }
